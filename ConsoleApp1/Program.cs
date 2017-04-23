f1a1d5e81c34a675d8cc4f1234367b3eb0bcc17f
Doug Moraes <dougmoraes91@gmail.com>
2017-04-23 03:23:07 -0300

    Commit inicial com otimizações

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace palindromos3digitos
+{
+    class Program
+    {
+        static int maior_palindromo = 0;
+
+        static void Main(string[] args)
+        {
+            for (int a = 100; a <= 999; a++)
+            {
+                for (int b = 100; b <= 999; b++)
+                {
+                    if (a >= b && a % 10 != 0 && b % 10 != 0 )
+                    {
+                        int produto = a * b;
+
+                        palindromo(produto);
+                    }
+
+                }
+            }
+
+            Console.WriteLine(maior_palindromo);
+            Console.ReadKey();
+        }
+
+        static bool palindromo(int produto)
+        {
+            int aux = produto;
+            int ult_digitido;
+            int invertido = 0;
+
+            while(aux != 0)
+            {
+                ult_digitido = aux % 10;
+                invertido = invertido * 10 + ult_digitido;
+                aux = aux / 10;
+            }
+
+            if (produto == invertido)
+            {
+                if (produto > maior_palindromo)
+                {
+                    maior_palindromo = produto;
+                    return true;
+                }
+
+                return false;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
